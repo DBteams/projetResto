ceee60de8702cafd8f639ce24d4c8aa20e70c5bc
mgrandbarbe <mgrandbarbe@la-joliverie.com>
2022-12-12 10:14:46 +0100

    Projet final

---
+++ sql/drop.sql
@@ -0,0 +1,11 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  DBbouf
+ * Created: 14 nov. 2022
+ */
+
+DROP DATABASE 2slamprj1eq07_resto2;
+
